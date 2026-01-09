b28f3e2adf6cfb050989628304d2d3f732d67a0a
Jeremy <jeremychristopherryan@gmail.com>
2026-01-09 11:40:49 -0800

    Update setup-windows.ts
    Add debug statement and install unity hub

--- src/model/platform-setup/setup-windows.ts
+++ src/model/platform-setup/setup-windows.ts
@@ -23,11 +23,14 @@ class SetupWindows {
         break;
       case 'StandaloneLinux64': {
         const command_lin = `"C:/Program Files/Unity Hub/Unity Hub.exe" -- --headless install-modules --version 6000.0.58f2 --module linux-mono`;
+
+        await exec(`echo JCR test StandaloneLinux64`, undefined, {silent: false, ignoreReturnCode: true});
+        await exec(`choco install unity-hub`, undefined, {silent: false, ignoreReturnCode: true});
         
         // Ignoring return code because the log seems to overflow the internal buffer which triggers
         // a false error
         const errorCode_lin = await exec(command_lin, undefined, {
-          silent,
+          silent: silent,
           ignoreReturnCode: true,
         });
         if (errorCode_lin) {
@@ -37,11 +40,14 @@ class SetupWindows {
       }
       case 'StandaloneOSX': {
         const command_osx = `"C:/Program Files/Unity Hub/Unity Hub.exe" -- --headless install-modules --version 6000.0.58f2 --module mac-mono`;
-    
+
+        await exec(`echo JCR test StandaloneOSX`, undefined, {silent: false, ignoreReturnCode: true});
+        await exec(`choco install unity-hub`, undefined, {silent: false, ignoreReturnCode: true});
+        
         // Ignoring return code because the log seems to overflow the internal buffer which triggers
         // a false error
         const errorCode_osx = await exec(command_osx, undefined, {
-          silent,
+          silent: silent,
           ignoreReturnCode: true,
         });
         if (errorCode_osx) {
